f129c609e4a24dfe8c2b341db1ab8dade955e626
Lawrene Jang <jkb2221@gmail.com>
2023-08-28 11:36:42 +0900

    feat: 선택정렬 구현

---
+++ 라이브러리/정렬/선택정렬.py
@@ -0,0 +1,11 @@
+# 가장 작은 데이터를 선택해 맨 앞에 있는 데이터와 바꾸고, 그다음 작은 데이터를 선택해 앞에서 두 번쨰 데이터와 바꾸는 과정을 반복하는 정렬
+array = [ 7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i # 가장 작은 원소의 인덱스
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i] # 스와프
+
+print(array)
