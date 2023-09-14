49d9bb4756acd657d1908ad5a58ba8d5379af0bd
Lawrene Jang <jkb2221@gmail.com>
2023-09-14 23:48:26 +0900

    feat: 미래 도시 플로이드 워셜 알고리즘 사용하여 풀이

---
+++ 라이브러리/최단 경로/미래 도시.py
@@ -0,0 +1,33 @@
+# 전체 회사의 개수 n, 경로의 개수 m 입력
+n, m = map(int, input().split())
+INF = int(1e9)
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    # A와 B가 서로에게 가는 비용은 1이라고 설정
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+# 거쳐 갈 노드 X와 최종 목적지 노드 K를 입력받기
+x, k = map(int, input().split())
+
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+distance = graph[1][k] + graph[k][x]
+
+if distance >= INF:
+    print('-1')
+else:
+    print(distance)
