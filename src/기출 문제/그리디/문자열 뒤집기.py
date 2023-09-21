0cca6e04a9f029c03e301ae11635acf2f16a3da3
Lawrene Jang <jkb2221@gmail.com>
2023-09-21 23:14:09 +0900

    feat: 문자열 뒤집기 답안과 비교하여 학습

--- src/기출 문제/그리디/문자열 뒤집기.py
+++ src/기출 문제/그리디/문자열 뒤집기.py
@@ -9,16 +9,23 @@
 
 s = input()
 
-count = 0
+count0 = 0  # 전부 0으로 바꾸는 경우
+count1 = 0  # 전부 1로 바꾸는 경우
 
-for i in range(1, len(s)):
-    if s[i - 1] != s[i]:
-        count += 1
-
-if count % 2 == 0:
-    print(int(count // 2))
+# 첫번쨰 원소 처리
+if s[0] == '1':
+    count0 += 1
 else:
-    print(int(count // 2) + 1)
+    count1 += 1
 
+# 두 번째 원소부터 모든 원소 확인하며
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        # 다음 수에서 1로 바뀌는 경우
+        if s[i + 1] == '1':
+            count0 += 1
+        # 다음 수에서 0으로 바뀌는 경우
+        else:
+            count1 += 1
 
-
+print(min(count0, count1))
