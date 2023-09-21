0fcc6be6e03d6002723dff7d2508e3fb8e8c4c08
Lawrene Jang <jkb2221@gmail.com>
2023-09-21 22:46:03 +0900

    feat: 곱하기 혹은 더하기 그리디 중복 코드 변수로 수정

--- 기출 문제/그리디/곱하기 혹은 더하기.py
+++ 기출 문제/그리디/곱하기 혹은 더하기.py
@@ -9,9 +9,10 @@ s = input()
 result = int(s[0])
 
 for i in range(1, len(s)):
+    num = int(s[i])
     if int(s[i - 1]) == 0:
-        result += int(s[i])
+        result += num
     else:
-        result *= int(s[i])
+        result *= num
 
 print(result)
